ebf32f70dc4d1a57621869aa0f5d8ab4e15359c6
Jmanu866 <130512420+Jmanu866@users.noreply.github.com>
2025-05-07 22:58:31 -0400

    fix: correct property names and formatting in DTO classes

--- TallerIdwm/src/dtos/AuthenticateUserDto.cs
+++ TallerIdwm/src/dtos/AuthenticateUserDto.cs
@@ -8,7 +8,7 @@ namespace TallerIdwm.src.dtos
 {
     public class AuthenticateUserDto
     {
-         public string FirstName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Phone { get; set; } = null!;
